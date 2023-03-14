8dd9fb62a83bece5b407af79985d4affdf69f604
Samir Jadhav <samirzjadhav@gmail.com>
2023-03-14 11:02:25 +0530

    modify files

---
+++ script.js
@@ -0,0 +1,90 @@
+const menu = document.getElementById("menu-bars");
+const nav = document.getElementById("nav");
+const searchIcon = document.getElementById("search-icons");
+const searchForm = document.getElementById("search-form");
+const close = document.getElementById("close");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  nav.classList.toggle("active");
+};
+
+let section = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header .navbar a");
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  nav.classList.remove("active");
+
+  section.forEach((sec) => {
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop - 150;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header .navbar a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
+
+searchIcon.onclick = () => {
+  searchForm.classList.toggle("active");
+};
+
+close.onclick = () => {
+  searchForm.classList.remove("active");
+};
+
+const swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
+
+const swiper1 = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+function loader() {
+  document.querySelector(".loader-container").classList.add("fade-out");
+}
+
+function fadeOut() {
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut;
